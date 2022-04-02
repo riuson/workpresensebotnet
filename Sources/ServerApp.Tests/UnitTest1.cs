a4d782cc26f77485317712d64fa7159db6381ca2
Vladimir <riuson@gmail.com>
2022-04-02 12:19:31 +0500

    Initial commit

---
+++ Sources/ServerApp.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+
+namespace ServerApp.Tests
+{
+    /// <summary>
+    /// Demo tests.
+    /// </summary>
+    public class Tests
+    {
+        /// <summary>
+        /// Setup.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Test1.
+        /// </summary>
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Sources/ServerApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ServerApp;
+
+/// <summary>
+/// Program entry class.
+/// </summary>
+internal class Program
+{
+    /// <summary>
+    /// Main entry point.
+    /// </summary>
+    /// <param name="args">Command line arguments.</param>
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
